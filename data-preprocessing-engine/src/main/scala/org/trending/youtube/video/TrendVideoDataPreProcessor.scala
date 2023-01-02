f90bda55d2d6c532093d4a26261a0a0782b0915b
adnanrahin <adnan.rahin13@gmail.com>
2023-01-02 02:35:39 -0500

    update config file

--- data-preprocessing-engine/src/main/scala/org/trending/youtube/video/TrendVideoDataPreProcessor.scala
+++ data-preprocessing-engine/src/main/scala/org/trending/youtube/video/TrendVideoDataPreProcessor.scala
@@ -2,7 +2,7 @@ package org.trending.youtube.video
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.trending.youtube.video.util.{DataWriter, DataFrameUtilMethods}
+import org.trending.youtube.video.util.{Constant, DataFrameUtilMethods, DataWriter}
 
 object TrendVideoDataPreProcessor {
 
@@ -15,21 +15,23 @@ object TrendVideoDataPreProcessor {
         .master("local[*]")
         .getOrCreate()
 
+    val inputSource: String = Constant.VIDEO_INFO_INPUT
+
     val csvFileDF = spark.read.format("csv")
       .option("header", "true")
       .option("delimiter", ",")
-      .load("/Users/adnanrahin/source-code/scala/big-data/Trending-YouTube-Video-Statistics/data-set/trending_youtube_video_statistics_dataset/videos_info/*.csv")
+      .load(inputSource)
 
     val countryCodeDF = csvFileDF
-      .withColumn("country_code",
+      .withColumn(Constant.COUNTRY_CODE,
         callUDF(DataFrameUtilMethods.getFileName(spark), input_file_name()))
 
     val countryCategoryCode =
       DataFrameUtilMethods
         .concatDataFrameColumns(
-          "country_category_code",
-          "country_code",
-          "category_id",
+          Constant.CATEGORY_COUNTRY_CODE,
+          Constant.COUNTRY_CODE,
+          Constant.CATEGORY_ID,
           "_",
           countryCodeDF
         )
@@ -37,7 +39,7 @@ object TrendVideoDataPreProcessor {
     DataWriter
       .dataWriter(
         countryCategoryCode,
-        "data-set/trending_youtube_video_statistics_dataset/",
+        Constant.VIDEO_INFO_OUTPUT,
         "videos_info_filter")
 
   }
---
+++ data-preprocessing-engine/src/main/scala/org/trending/youtube/video/util/Constant.scala
@@ -0,0 +1,10 @@
+package org.trending.youtube.video.util
+
+object Constant {
+
+  final val VIDEO_INFO_INPUT = "/Users/adnanrahin/source-code/scala/big-data/Trending-YouTube-Video-Statistics/data-set/trending_youtube_video_statistics_dataset/videos_info/*.csv"
+  final val VIDEO_INFO_OUTPUT = "/Users/adnanrahin/source-code/scala/big-data/Trending-YouTube-Video-Statistics/data-set/trending_youtube_video_statistics_dataset/"
+  final val CATEGORY_COUNTRY_CODE = "country_category_code"
+  final val COUNTRY_CODE = "country_code"
+  final val CATEGORY_ID = "category_id"
+}
