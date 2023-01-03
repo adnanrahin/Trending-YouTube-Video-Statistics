42bbdefb7d65b69336ebf21d4dff9565e642c2b8
adnanrahin <adnan.rahin13@gmail.com>
2023-01-03 17:42:04 -0500

    Case class created VideoCategroyID

---
+++ data-scoring-processor/src/main/scala/org/youtube/trending/scoring/schemas/VideoCategoryIdSchema.scala
@@ -0,0 +1,14 @@
+package org.youtube.trending.scoring.schemas
+
+case class VideoCategoryIdSchema(
+                                  etag: String,
+                                  kind: String,
+                                  itemsEtag: String,
+                                  itemsId: String,
+                                  itemsKind: String,
+                                  itemSnippetsAssignable: String,
+                                  itemSnippetsChannelId: String,
+                                  itemSnippetsTable: String,
+                                  countryCode: String,
+                                  countryCategoryCode: String
+                                )
