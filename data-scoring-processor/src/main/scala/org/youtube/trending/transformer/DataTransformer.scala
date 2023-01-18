d8bc1a97244cde95ad908c48ae0b2f27fbf2a040
adnanrahin <adnan.rahin13@gmail.com>
2023-01-17 19:53:24 -0500

    update string trimming logic

--- data-scoring-processor/src/main/scala/org/youtube/trending/transformer/DataTransformer.scala
+++ data-scoring-processor/src/main/scala/org/youtube/trending/transformer/DataTransformer.scala
@@ -13,28 +13,29 @@ trait DataTransformer {
     import spark.implicits._
     val videoInfoDataRDD: RDD[VideoInfoSchema] =
       spark.read.parquet(inputPath)
+        .filter(row => !row.toString().startsWith("\n"))
         .map(
 
           videoInfoAttribute => {
 
-            val videoID: String = if (videoInfoAttribute(0) == null) "empty" else videoInfoAttribute(0).toString
-            val trendingDate: String = if (videoInfoAttribute(1) == null) "empty" else videoInfoAttribute(1).toString
-            val title: String = if (videoInfoAttribute(2) == null) "empty" else videoInfoAttribute(2).toString
-            val channelTitle: String = if (videoInfoAttribute(3) == null) "empty" else videoInfoAttribute(3).toString
-            val categoryId: String = if (videoInfoAttribute(4) == null) "empty" else videoInfoAttribute(4).toString
-            val publishTime: String = if (videoInfoAttribute(5) == null) "empty" else videoInfoAttribute(5).toString
-            val tags: String = if (videoInfoAttribute(6) == null) "empty" else videoInfoAttribute(6).toString
+            val videoID: String = if (videoInfoAttribute(0) == null) "empty" else videoInfoAttribute(0).toString.replaceAll("\n", " ").trim
+            val trendingDate: String = if (videoInfoAttribute(1) == null) "empty" else videoInfoAttribute(1).toString.replaceAll("\n", " ").trim
+            val title: String = if (videoInfoAttribute(2) == null) "empty" else videoInfoAttribute(2).toString.replaceAll("\n", " ").trim
+            val channelTitle: String = if (videoInfoAttribute(3) == null) "empty" else videoInfoAttribute(3).toString.replaceAll("\n", " ").trim
+            val categoryId: String = if (videoInfoAttribute(4) == null) "empty" else videoInfoAttribute(4).toString.replaceAll("\n", " ").trim
+            val publishTime: String = if (videoInfoAttribute(5) == null) "empty" else videoInfoAttribute(5).toString.replaceAll("\n", " ").trim
+            val tags: String = if (videoInfoAttribute(6) == null) "empty" else videoInfoAttribute(6).toString.replaceAll("\n", " ").trim
             val views: Long = if (videoInfoAttribute(7) != null && UtilMethods.isNumeric(videoInfoAttribute(7).toString)) videoInfoAttribute(7).toString.replaceAll("[^0-9]", "").toLong else 0L
             val likes: Long = if (videoInfoAttribute(8) != null && UtilMethods.isNumeric(videoInfoAttribute(8).toString)) videoInfoAttribute(8).toString.replaceAll("[^0-9]", "").toLong else 0L
             val dislikes: Long = if (videoInfoAttribute(9) != null && UtilMethods.isNumeric(videoInfoAttribute(9).toString)) videoInfoAttribute(9).toString.replaceAll("[^0-9]", "").toLong else 0L
             val commentCount: Long = if (videoInfoAttribute(10) != null && UtilMethods.isNumeric(videoInfoAttribute(10).toString)) videoInfoAttribute(10).toString.replaceAll("[^0-9]", "").toLong else 0L
-            val thumbnailLink: String = if (videoInfoAttribute(11) == null) "empty" else videoInfoAttribute(11).toString
+            val thumbnailLink: String = if (videoInfoAttribute(11) == null) "empty" else videoInfoAttribute(11).toString.replaceAll("\n", " ").trim
             val commentsDisable: Boolean = if (videoInfoAttribute(12) != null && videoInfoAttribute(12).toString.trim.equalsIgnoreCase("False")) false else true
             val ratingsDisable: Boolean = if (videoInfoAttribute(13) != null && videoInfoAttribute(13).toString.trim.equalsIgnoreCase("False")) false else true
             val videoErrorOrRemoved: Boolean = if (videoInfoAttribute(14) != null && videoInfoAttribute(14).toString.trim.equalsIgnoreCase("False")) false else true
-            val description: String = if (videoInfoAttribute(15) == null) "empty" else videoInfoAttribute(15).toString
-            val countryCode: String = if (videoInfoAttribute(16) == null) "empty" else videoInfoAttribute(16).toString
-            val countryCategoryCode: String = if (videoInfoAttribute(17) == null) "empty" else videoInfoAttribute(17).toString
+            val description: String = if (videoInfoAttribute(15) == null) "empty" else videoInfoAttribute(15).toString.replaceAll("\n", " ").trim
+            val countryCode: String = if (videoInfoAttribute(16) == null) "empty" else videoInfoAttribute(16).toString.replaceAll("\n", " ").trim
+            val countryCategoryCode: String = if (videoInfoAttribute(17) == null) "empty" else videoInfoAttribute(17).toString.replaceAll("\n", " ").trim
 
             VideoInfoSchema(
               videoID: String,
